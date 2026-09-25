be417fe5ae8e76dcb9b1ffdb647af37287a6853d
kimjintae <verbal0719@naver.com>
2026-09-25 17:02:16 +0900

    feat : share spaceship interaction

---
+++ Assets/PoC/Spaceship/Scripts/MutiScene/PocInteractableViewer.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+public abstract class PocInteractableViewer : MonoBehaviour
+{
+    [SerializeField] protected TextMeshPro stateText;
+
+    private bool isNearby;
+
+    protected abstract string ActionText { get; }
+
+    protected virtual void Start()
+    {
+        Refresh();
+    }
+
+    public void ShowPrompt(bool visible)
+    {
+        isNearby = visible;
+        Refresh();
+    }
+
+    protected void ShowState(string state)
+    {
+        if (stateText != null)
+        {
+            stateText.text = isNearby ? $"{state}\n[E] {ActionText}" : state;
+        }
+    }
+
+    public abstract void Interact();
+    public abstract void Refresh();
+}
